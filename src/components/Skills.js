8a64502dc3b2869e2b76fe58b84835677842d5b7
David Caldera <davidcaldera13@yahoo.com>
2022-07-29 14:44:25 -0500

    import skill images, populate about, start skills

---
+++ src/components/Skills.js
@@ -0,0 +1,27 @@
+import CSS from '../assets/css.png'
+import HTML from '../assets/html5.png'
+import JS from '../assets/js.png'
+import NET from '../assets/net.png'
+import Rails from '../assets/rails.png'
+import React from '../assets/react.png'
+import Tailwind from '../assets/tailwind.png'
+
+function Skills() {
+    return (
+        <div name="skills">
+            <div>
+                <div>
+                    <p>Experience</p>
+                    <p>These are the technologies I work with</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
