306529bc8fed88a883719c76f00690f1fe5a5451
David Caldera <davidcaldera13@yahoo.com>
2022-08-18 13:24:09 -0500

    added work, added contact form

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+function Contact() {
+  return (
+    <div
+      name="contact"
+      className="bg-slate-500 w-full h-screen flex justify-center items-center p-4"
+    >
+      <form method="POST" action="https://getform.io/f/6cd11906-20af-44e9-889c-007115cc0f5e" className="flex flex-col max-w-[600px] w-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-800">Contact</p>
+          <p>Submit the form below or email me - david.a.caldera97@gmail.com</p>
+        </div>
+        <input className="p-2"type="text" placeholder="Name" name="name"></input>
+        <input className="my-4 p-2"type="email" placeholder="Email" name="name"></input>
+        <textarea className="p-2"name="message" rows="10" placeholder="Message"></textarea>
+        <button className="border-2 px-4 py-3 my-8 mx-auto flex items-center duration-500 hover:bg-gradient-to-r from-slate-400 to-slate-800 ">Contact Me</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,25 +1,64 @@
-import FlatBook from '../assets/flatbook.png'
-import Archive from '../assets/the_archive.png'
-import Forum from '../assets/TheForum.png'
+import FlatBook from "../assets/flatbook.png";
+import Archive from "../assets/the_archive.png";
+import Forum from "../assets/TheForum.png";
 function Work() {
   return (
     <div className="w-full md:h-screen bg-slate-500">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-800">Work</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-800">
+            Work
+          </p>
           <p className="py-6">Heres some of my recent work</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div className="shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
-            <div>
-                {/* add the portfolio screenshots now */}
-              <span></span>
-              <div>
+          <div
+            style={{ backgroundImage: `url(${FlatBook})` }}
+            className="shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100 duration-500">
+              <span className="text 2xl font-bold text-white tracking-wider">
+                React Js and Ruby on Rails Application
+              </span>
+              <div className="pt-8 text-center">
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-800 font-bold text-lg">
+                    code{" "}
+                  </button>
                 </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Archive})` }}
+            className="shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100 duration-500">
+              <span className="text 2xl font-bold text-white tracking-wider">
+                Vanilla JS art viewer
+              </span>
+              <div className="pt-8 text-center">
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-800 font-bold text-lg">
+                    code{" "}
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Forum})` }}
+            className="shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100 duration-500">
+              <span className="text 2xl font-bold text-white tracking-wider">
+                React Js and Ruby on Rails Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-800 font-bold text-lg">
+                    code{" "}
+                  </button>
                 </a>
               </div>
             </div>
